db6685985e1c6e3784570f50ceea1bf1fce0c485
GCC <gechenchen@peilian.com>
2020-11-30 16:34:18 +0800

    fix

--- src/main/java/com/mall/comm/vo/in/email/SendEmailIn.java
+++ src/main/java/com/mall/comm/vo/in/email/SendEmailIn.java
@@ -4,5 +4,15 @@ import lombok.Data;
 
 @Data
 public class SendEmailIn {
+
+    /**
+     * 主题
+     */
+    private String subject;
+
+    /**
+     * 发信地址，多个用，隔开
+     */
+    private String toAddress;
     
 }
